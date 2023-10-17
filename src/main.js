f8de3a8723d73a39ab024de54575ec60a8a43162
Sammuss <445313282@qq.com>
2023-10-17 11:03:44 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import 'animate.css'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import { createApp } from 'vue'
-
 import axios from './lib/axios'
+
 import elementPlus from './lib/element-plus'
 import i18n from './lib/i18n'
 import router from './lib/router'
